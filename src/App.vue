9605f4a498b41140eb4e760e7e3d21b2f5c2436c
emir <contact@ekherchi.fr>
2020-12-08 09:46:54 +0100

    first commit
    vue / bstrp / router / emailJs done

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!-- As a heading -->
+    <b-navbar sticky toggleable="sm" variant="faded" type="light">
+      <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+      <b-navbar-brand  tag="h1" to="/" class="mb-0">
+        <b-img
+        fluid
+          src="./assets/images/Original.svg"
+          class="d-inline-block align-top"
+          alt="Logo entreprise isolation 1euro en eco normandie"
+        />
+      </b-navbar-brand>
+      <b-collapse id="nav-text-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/about">About</b-nav-item>
+          <b-nav-item to="/contact">contact</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+   
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  img {
+   max-width: 100px;
+  }
+ #nav-text-collapse{
+  margin-left: 35%;
+    a{
+  margin-right: 100px;
+}
+  }
+
+}
+</style>
---
+++ src/components/FormContact.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="formContact">
+    <h1>{{ title }}</h1>
+    <form class="contact-form" @submit.prevent="sendEmail">
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message"></textarea>
+      <input type="submit" value="Send" />
+    </form>
+  </div>
+</template>
+
+<script>
+import emailjs from 'emailjs-com';
+
+export default {
+  name: "Test",
+  props: {
+    title: String,
+  },
+  methods: {
+    sendEmail: (e) => {
+      emailjs.sendForm('service_d7yzkun', 'template_suls7od', e.target, 'user_DjSPAKIEOISYGSAvrYotn')
+        .then((result) => {
+            console.log('SUCCESS!', result.status, result.text);
+        }, (error) => {
+            console.log('FAILED...', error);
+        });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
