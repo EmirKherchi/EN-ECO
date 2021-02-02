95da9890e261cfe3ba7a7d44b4ad34b23f822fb4
emir <contact@ekherchi.fr>
2021-02-02 13:24:46 +0100

    hero banner

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,72 @@
 <template>
   <div id="app">
     <!-- As a heading -->
-    <b-navbar sticky toggleable="md" variant="faded" type="light">
-      <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
-      <b-navbar variant="faded" type="light">
-        <b-navbar-brand to="/">
-          <b-img
-            fluid
-            src="./assets/images/logo.png"
-            class="d-inline-block align-top"
-            alt="Logo entreprise isolation 1euro en eco normandie"
-          />
-        </b-navbar-brand>
+    <b-container fluid class="header-nav">
+      <b-navbar sticky toggleable="md" type="light">
+        <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+        <b-navbar variant="faded" type="light">
+          <b-navbar-brand to="/">
+            <b-img
+              fluid
+              src="./assets/images/logo.png"
+              class="d-inline-block align-top"
+              alt="Logo entreprise isolation 1euro en eco normandie"
+            />
+          </b-navbar-brand>
+        </b-navbar>
+
+        <b-collapse id="nav-text-collapse" class="justify-content-end" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/works">Nos Réalisations</b-nav-item>
+            <b-nav-item to="/contact">Contact</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
       </b-navbar>
-
-      <b-collapse id="nav-text-collapse" class="justify-content-end" is-nav>
-        <b-navbar-nav >
-          <b-nav-item to="/works">Nos Réalisations</b-nav-item>
-          <b-nav-item to="/contact">Contact</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-
-    
+    </b-container>
 
     <router-view />
   </div>
-
-  
 </template>
 
 <style lang="scss">
+body {
+  background-color: #fcfcfc;
+}
 #app {
-  body {
-    background-color: #fcfcfc;
+  .header-nav{
+    background-color: #fcfcfcf6;
+    position: sticky!important;
+    top: 0px;
+    z-index: 100000;
+    
   }
   img {
     max-width: 120px;
   }
   #nav-text-collapse {
-    //margin-left: 40%;
     a {
       margin-right: 10px;
     }
   }
   .navbar-light .navbar-nav .nav-link {
     color: #111;
-    font-weight: 500;
+    font-weight: 600;
+    letter-spacing: 1px;
+    border-bottom: 0 solid #285b84;
+    transition: border-bottom 100ms ease-in-out,
+      padding-bottom 100ms ease-in-out;
+    &:hover {
+      padding-bottom: 0.1rem;
+      border-bottom: 0.1rem solid #285b84;
+    }
+    & > :first-child {
+      margin-top: 0;
+    }
+    & > :last-child {
+      margin-top: 0;
+    }
   }
+
   .sticky-top {
     padding-bottom: 50px;
     padding-top: 50px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,85 @@
 <template>
   <div class="home">
-   
-   
+    <div>
+      <b-jumbotron
+        class="xyz-in"
+        xyz="fade-100% duration-5 ease-in-out"
+        :style="{
+          backgroundImage:
+            'url(' + require('../assets/images/home_jumbotron.png') + ')',
+        }"
+        header="EN ECO NORMANDIE ISOLATION"
+        lead="Société Délégatrice des CEE à 1 Euro"
+      >
+        <b-button
+          class="xyz-in"
+          xyz="fade-100% duration-15 ease-in-out delay-3 down-5"
+          variant="primary"
+          to="/contact"
+          >Demandez votre devis</b-button
+        >
+      </b-jumbotron>
+    </div>
+    <b-container class="card_home">
+      <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-right>
+      <b-card-text>
+        Some quick example text to build on the card and make up the bulk of the card's content.
+      </b-card-text>
+    </b-card>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
-
+import "@animxyz/core";
 export default {
-  name: 'Home',
-
-}
+  name: "Home",
+};
 </script>
+
+<style lang="scss" scoped>
+.home {
+  margin-top: 50px;
+  .jumbotron {
+    border-radius: 0;
+    text-align: center;
+    color: #fbfbfd;
+    background-size: cover;
+    height: 600px;
+    overflow: hidden;
+    h1 {
+      margin-top: 80px;
+      font-weight: 900;
+      letter-spacing: 1px;
+    }
+    .lead {
+      font-weight: 100;
+      opacity: 0.7;
+      letter-spacing: 1px;
+      margin-bottom: 50px;
+    }
+    .btn-primary {
+      color: #fff;
+      background-color: #5ea669;
+      border: 1px solid #5ea669;
+
+      &:hover {
+        background-color: transparent;
+        border-color: #fff;
+        transition: all 600ms ease;
+      }
+    }
+    @media only screen and (max-width: 600px) {
+      h1 {
+        font-size: 3rem;
+      }
+    }
+  }
+  .card_home{
+    
+    display: block;
+    margin: 200px auto ;
+  }
+}
+</style>
