f59c9145ada7a789c63d5f179cf090fc88627c67
emir <contact@ekherchi.fr>
2021-02-05 12:51:17 +0100

    form contact method done

--- src/components/FormContact.vue
+++ src/components/FormContact.vue
@@ -1,53 +1,82 @@
 <template>
   <div class="formContact">
-    <h1>{{ title }}</h1>
     <form class="contact-form" @submit.prevent="sendEmail">
       <label>Name</label>
       <input type="text" name="user_name" />
       <label>Email</label>
       <input type="email" name="user_email" />
+      <label for="iso_comble">Isolation des combles</label>
+      <input
+        type="checkbox"
+        name="iso_comble"
+        id="iso_comble"
+        value="isolation des combles"
+      />
+      <label for="iso_sous">Isolation du sous-sol</label>
+      <input
+        type="checkbox"
+        name="iso_sous"
+        id="iso_sous"
+        value="isolation du sous sol"
+      />
       <label>Message</label>
       <textarea name="message"></textarea>
+
       <input type="submit" value="Send" />
+      
     </form>
   </div>
 </template>
 
 <script>
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 
 export default {
   name: "FormContact",
   props: {
     title: String,
   },
+  data: function() {
+    return {
+      isocomble: false,
+    };
+  },
   methods: {
     sendEmail: (e) => {
-      emailjs.sendForm('service_d7yzkun', 'template_suls7od', e.target, 'user_DjSPAKIEOISYGSAvrYotn')
-        .then((result) => {
-            console.log('SUCCESS!', result.status, result.text);
-        }, (error) => {
-            console.log('FAILED...', error);
-        });
-    }
-  }
+      emailjs
+        .sendForm(
+          "service_d7yzkun",
+          "template_suls7od",
+          e.target,
+          "user_DjSPAKIEOISYGSAvrYotn"
+        )
+        .then(
+          (result) => {
+            console.log("SUCCESS!", result.status, result.text);
+          },
+          (error) => {
+            console.log("FAILED...", error);
+          }
+        );
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss" scoped>
+.formContact {
+  text-align: center;
+  input {
+    display: block;
+    margin: auto;
+    &[type="submit"] {
+      margin-top: 50px;
+    }
+  }
+  textarea {
+    display: block;
+    margin: auto;
+  }
 }
 </style>
